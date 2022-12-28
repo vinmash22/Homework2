0bf93618b1d8ed1993e764d057a33a2c55987b6e
vinmash <vinmash@mail.ru>
2022-12-28 23:27:16 +0500

    Task 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
         var dog = 8.0;
         System.out.println(dog);
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
         var cat = 3.6;
         System.out.println(cat);
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
         var paper = 763789;
         System.out.println(paper);
     }
