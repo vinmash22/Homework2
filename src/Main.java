731ef6c8898f4b3569a6679554540692fdc53cfe
vinmash <vinmash@mail.ru>
2022-12-28 23:36:40 +0500

    Task 1-3 correct

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,34 @@ public class Main {
         System.out.println("Задача 1");
         var dog = 8.0;
         System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
     }
     public static void task2 () {
         System.out.println("Задача 2");
+        var dog = 8.0;
+        dog = dog + 2;
+        System.out.println(dog);
         var cat = 3.6;
+        cat = cat + 2;
         System.out.println(cat);
+        var paper = 763789;
+        paper = paper + 2;
+        System.out.println(paper);
+
     }
     public static void task3 () {
-        System.out.println("Задача 3");
+      System.out.println("Задача 3");
+        var dog = 8.0;
+        dog = dog - 3.5;
+        System.out.println(dog);
+        var cat = 3.6;
+        cat = cat - 1.6;
+        System.out.println(cat);
         var paper = 763789;
+        paper = paper - 7639;
         System.out.println(paper);
     }
 }
