b6d588b4b4650b73e4392e11d0f97e906227f324
vinmash <vinmash@mail.ru>
2022-12-28 23:41:15 +0500

    Task 1-4

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
     public static void task1 () {
         System.out.println("Задача 1");
@@ -37,5 +38,14 @@ public class Main {
         var paper = 763789;
         paper = paper - 7639;
         System.out.println(paper);
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
